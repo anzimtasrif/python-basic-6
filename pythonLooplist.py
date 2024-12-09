747af3693e4479540957f596618f2fd708978664
Anzim Tasrif <109681624+anzimtasrif@users.noreply.github.com>
2024-12-10 01:08:46 +0600

    Add files via upload

---
+++ pythonLooplist.py
@@ -0,0 +1,13 @@
+#python Loop List
+LoopList = ["farcry","Witcher","apple","orange"]
+
+for Name in LoopList:
+    print(Name)
+
+#While Looplist
+LoopList = ["farcry","Witcher","apple","orange"]
+y = 0
+
+while y < len(LoopList):
+    print(LoopList[y]) 
+    
